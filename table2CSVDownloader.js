af05af444aa1f53736574428455251befeebbaf5
Justine <justine@shareprogress.org>
2015-03-16 16:52:31 -0700

    initial commit

---
+++ table2CSVDownloader.js
@@ -0,0 +1,37 @@
+if (window.jQuery) { 
+    loadScript();
+    
+} else {
+    var jq = document.createElement('script');
+    jq.type = 'text/javascript'; 
+    jq.onload = function() { 
+        console.log('jQuery loading');
+        loadScript();
+    }   
+    jq.src = 'http://ajax.googleapis.com/ajax/libs/jquery/1.11.1/jquery.js';
+    document.getElementsByTagName('head')[0].appendChild(jq);
+}
+
+function loadScript() {
+    var A = document.createElement('script');
+    A.type = 'text/javascript'; 
+    A.onload = function() { 
+        console.log('Script loading');
+    }   
+    A.src = 'http://scripts-live.shpg.org/TabletoCSVbookmarklet.js';
+    document.getElementsByTagName('head')[0].appendChild(A);
+    mouseActions();
+}
+
+function mouseActions() {
+    jQuery('table').mouseover( function() {
+       $(this).css('border','5px black solid');
+    });
+    jQuery('table').mouseleave( function() {
+        $(this).css('border', 'none');
+    });
+    jQuery('table tbody').click( function() {
+        var table = $(this)[0];
+        tableData(table);
+  })
+}
