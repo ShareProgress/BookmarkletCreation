7dff2adf969617e5d817b9367326b3b208a3bb2f
Justine <justine@shareprogress.org>
2015-03-18 11:40:38 -0700

    Fixed issue with double quotes in first column.

--- TabletoCSVbookmarklet.js
+++ TabletoCSVbookmarklet.js
@@ -1,17 +1,18 @@
 
-function findPosition(el) {
-    pos = el.position();
-    console.log(pos);
-}
+
 function tableData(table) {
-    var tr = $(table).find('tr');
+    var $ = jQuery;
+    var tr = $(table).children();
     var tableString = "";
 
+
     var tableData = $.map(tr, function(tr) {
-     return [$.map($(tr), function(td) {
-        return $(td).children().text();
-     })];
+     var row = [$.map($(tr).children(), function(td) {
+                    return $(td).text();
+                })];
+     return row;
     });
+    
     
     // for each element in each array find ones with a comma and escape it with "/"
     tableData.forEach(function(entry) {
@@ -30,19 +31,21 @@ function tableData(table) {
         tableString += i;
         tableString += '",';
       })
+
        //remove the last comma in a row so that there isn't an extra column
        tableString = tableString.substring(0, tableString.length - 1);
-       tableString += "%0A ";
+       tableString += "%0A";
     });    
     
-    console.log(tableData);
+
 
     // download as CSV file  //
  
     var a = document.createElement('a');
-    a.href        = 'data:text/csv;charset=utf-8,' + tableString;
+    a.href        = 'data:text/csv,' + tableString;
     a.target      = '_blank';
     a.download    = "tabledownload.csv";
     document.body.appendChild(a);
     a.click();
 }
+
