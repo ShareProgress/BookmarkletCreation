5b46d464dceaceee6be2baa24d0ac4c565feba67
Justine <justine@shareprogress.org>
2015-03-24 10:20:25 -0700

    Refactoring script file

--- TabletoCSVbookmarklet.js
+++ TabletoCSVbookmarklet.js
@@ -10,21 +10,25 @@ function init() {
        $$(this).css("cursor","pointer");
        //if table-overlay already exists show it
        if ($$( "div.table-overlay" ).length ) {
-        $$( "div.table-overlay" ).show();
+        $$( "div.table-overlay" ).remove();
        } else {
         // otherwise add a div element with class "table-overlay" to the body element
-           $$('body').append($$("<div></div>").addClass("table-overlay"));
-           $$('div.table-overlay').append("<div>Click to download table as CSV file</div>");
+           
         }
 
        //set position, height, width, color of table-overlay to match the table that is hovered over.
+       var tableOverlay = $$("<div></div>");  
+       tableOverlay = tableOverlay.addClass('table-overlay');
 
-       $$('div.table-overlay').offset(pos);
-       $$('div.table-overlay').width(width);
-       $$('div.table-overlay').height(height);
-       $$('div.table-overlay').css({"position": "relative", "background-color": "rgba(0,0,0,0.4)", "z-index":100000, "pointer-events": "none", "cursor": "pointer","display": "table"});
-       
-       $$('div.table-overlay div').css({"color": "white", "pointer-events": "none","display": "table-cell", "text-align": "center", "vertical-align": "middle", "font-size": "26px", "font-family": "Arial", "font-weight": "800"});
+       tableOverlay.offset(pos);
+       tableOverlay.width(width);
+       tableOverlay.height(height);
+       tableOverlay.css({"position": "absolute", "background-color": "rgba(0,0,0,0.4)", "z-index":100000, "pointer-events": "none", "cursor": "pointer","display": "table"});
+       tableOverlay =  tableOverlay.append("<div></div>");
+
+       tableOverlay.children('div').text('Click to download table as CSV file').css({"color": "white", "pointer-events": "none","display": "table-cell", "text-align": "center", "vertical-align": "middle", "font-size": "26px", "font-family": "Arial", "font-weight": "800"});
+
+       $$('body').append(tableOverlay); 
         
 
     });
@@ -32,26 +36,22 @@ function init() {
       //when mouse leaves the table, hide the div.table-overlay element.
         $$('div.table-overlay').hide();
     });
-    $$('table tbody').click( function() {
+    $$('table').click( function() {
       // when table is clicked, start processing the data in table to be downloaded
-        var table = $$(this)[0];
+        var table = $$(this);
         tableData(table);
   })
-}function csvNormalDownload(str) {
-    // download table as CSV file  //
-    var a = document.createElement('a');
-    a.href        = 'data:text/csv,' + str;
-    a.target      = '_blank';
-    a.download    = "tabledownload.csv";
-    document.body.appendChild(a);
-    a.click();
 }
 
-function csvUTF16Download(str) {
-    // download table as UTF-16 charset CSV file, for Excel //
-    
+function csvDownload(str, utf) {
+    // download table as CSV file  //
     var a = document.createElement('a');
-    a.href        = 'data:text/csv;charset=utf-16,' + str;
+    if (utf !== false) {
+      a.href        = 'data:text/csv,' + str;
+    } else {
+      // if UTF == true download table as UTF-16 charset CSV file, for Excel //
+      a.href        = 'data:text/csv;charset=utf-16,' + str;
+    }
     a.target      = '_blank';
     a.download    = "tabledownload.csv";
     document.body.appendChild(a);
@@ -60,62 +60,58 @@ function csvUTF16Download(str) {
 
 // check whether the table contains UTF-16 characters and determine what type of file should be downloaded
 function utf16Checker(str) {
-
-    var re = /%u\d/;
-    var string = escape(str);
-    var res = string.search(re);
-    if ( res === -1) {
-        csvNormalDownload(str);
-    } else {
-        csvUTF16Download(str);
-        alert("NOTE: Because this table contains special characters, the data has been downloaded in UTF-16 format. This may cause compatibility issues with some spreadsheet programs.");
-    }
+  var re = /%u\d/;
+  var string = escape(str);
+  var res = string.search(re);
+  return (res !== -1);  
 }
 
 
 function tableData(table) {
+  var tr = table.find('> tr, > tbody > tr');  
+  var tableString = "";
 
-    var tr = $$(table).children();
-    var tableString = "";
+  var tableData = $$.map(tr, function(tr) {
+   var row = [$$.map($$(tr).children(), function(td) {
+    return $$(td).text();
+  })];
+   return row;
+ });
+  
+  
+  tableData.forEach(function(entry) {
+ 
+    entry.forEach(function(i) {
+      //for each cell's content escape spaces, double quotes and breaks
+      var re = new RegExp(" ", "g"),
+          rdq = new RegExp('"', "g"),
+          rlb = new RegExp("\n","g");
+      i = i.trim();
+      i = i.replace(re, "%20");
+      i = i.replace(rdq, '\"');
+      i = i.replace(rlb,'%0D');
 
-    var tableData = $$.map(tr, function(tr) {
-     var row = [$$.map($$(tr).children(), function(td) {
-                    return $$(td).text();
-                })];
-     return row;
-    });
-    
-    
-    tableData.forEach(function(entry) {
-   
-      entry.forEach(function(i) {
-        //for each cell's content escape spaces, double quotes and breaks
-        var space = " ",
-            re = new RegExp(space, "g"),
-            doublequotes = '"';
-            linebreak = "\n";
-            rdq = new RegExp(doublequotes, "g");
-            rlb = new RegExp(linebreak,"g");
-        i = i.trim();
-        i = i.replace(re, "%20");
-        i = i.replace(rdq, '\""');
-        i = i.replace(rlb,'%0D');
+      //add quotes around entire cell so that commas won't split the cell
+      tableString += '"';
+      tableString += i;
+      tableString += '",';
+      
+    })
 
-        //add quotes around entire cell so that commas won't split the cell
-        tableString += '"';
-        tableString += i;
-        tableString += '",';
-        
-      })
+    //remove the last comma in a row so that there isn't an extra column and add a line break 
+    tableString = tableString.substring(0, tableString.length - 1);
+    tableString += "%0A";
+     
+  }); 
 
-       //remove the last comma in a row so that there isn't an extra column
-       tableString = tableString.substring(0, tableString.length - 1);
-       tableString += "%0A";
-       
-    }); 
-    utf16Checker(tableString); 
+  if (utf16Checker(tableString)) {
+    csvDownload(tableString, true);
+    alert("NOTE: Because this table contains special characters, the data has been downloaded in UTF-16 format. This may cause compatibility issues with some spreadsheet programs.");
+  } else {
+    csvDownload(tableString, false);
+  }   
 }
-
+init();
 
 
     
